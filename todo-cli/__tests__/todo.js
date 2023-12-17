3d2fb5f805b1d7c4d7570fcdb9a654a4d0c7b324
admirer77 <109015913+admirer77@users.noreply.github.com>
2023-12-17 18:36:09 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,48 +1,33 @@
 const todoList = require('../todo');
 
-const { all, markAsComplete, add, getOverdue } = todoList(); // Update functions based on actual implementation
+const {all, markAsComplete, add } = todoList();
 
 describe("Todolist Test Suite", () => {
-  beforeAll(() => {
-    add({
-      title: "new todo",
-      completed: false,
-      dueDate: new Date().toISOString(), // Use ISO format for consistent date comparisons
+    beforeAll(() => {
+        add(
+            {
+                title: "new todo",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA")
+            }
+         );
+    })
+
+    test("Should add new todo", () => {
+         const todoItemsCount = all.length;
+         add(
+            {
+                title: "Test todo",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA")
+            }
+         );
+         expect(all.length).toBe(todoItemsCount + 1);
     });
-  });
 
-  // Existing tests
-  test("Should add new todo", () => {
-    const todoItemsCount = all.length;
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toISOString(),
-    });
-    expect(all.length).toBe(todoItemsCount + 1);
-  });
-
-  test("Should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-
-  // New test case with fix
-
-  test("Should retrieve overdue items", () => {
-    const overdueDate = new Date();
-    overdueDate.setDate(overdueDate.getDate() - 1);
-    add({
-      title: "Overdue todo",
-      completed: false,
-      dueDate: overdueDate.toISOString(),
-    });
-    const overdueItems = getOverdue();
-    expect(overdueItems.length).toBeGreaterThanOrEqual(1);
-    expect(overdueItems[0].title).toBe("Overdue todo");
-  });
-
-  // Add tests for getDueToday and getDueLater based on your actual implementation
-
-});
+    test("Should mark a todo as complete", () => {
+        expect(all[0].completed).toBe(false);
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true);
+    })
+})
