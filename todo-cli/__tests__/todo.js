7b996bc608f5ad4706adfe11996b36cd06861aa0
admirer77 <109015913+admirer77@users.noreply.github.com>
2023-12-18 21:47:24 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -46,7 +46,7 @@ describe("Todolist Test Suite", () => {
 
     test("Should retrieve due today items", () => {
         const dueTodayItems = todoInstance.dueToday();
-        expect(dueTodayItems.length).toBeGreaterThan(-1);
+        expect(dueTodayItems.length).toBeGreaterThan(0);
         // Add more specific assertions based on your implementation
     });
 
