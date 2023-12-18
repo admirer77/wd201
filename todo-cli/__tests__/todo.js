f19f2980f71cda7bccc8c7d2048a763b4e71ed29
admirer77 <109015913+admirer77@users.noreply.github.com>
2023-12-18 18:59:11 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -29,5 +29,29 @@ describe("Todolist Test Suite", () => {
         expect(all[0].completed).toBe(false);
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
+    });
+
+    test("Should retrieve overdue items", () => {
+      const now = new Date();
+      const overdueItems = overdue();
+    
+      // Check if all overdue items have due dates before today
+      expect(overdueItems.every((item) => item.dueDate < now)).toBe(true);
+    });
+
+    test("Should retrieve due today items", () => {
+      const now = new Date();
+      const dueTodayItems = dueToday();
+    
+      // Check if all due today items have due dates equal to today
+      expect(dueTodayItems.every((item) => item.dueDate.getFullYear() === now.getFullYear() && item.dueDate.getMonth() === now.getMonth() && item.dueDate.getDate() === now.getDate())).toBe(true);
+    });
+
+    test("Should retrieve due later items", () => {
+      const now = new Date();
+      const dueLaterItems = dueLater();
+    
+      // Check if all due later items have due dates after today
+      expect(dueLaterItems.every((item) => item.dueDate > now)).toBe(true);
     })
 })
